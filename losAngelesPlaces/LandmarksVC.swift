74f4335921041a3ca835660880eee899c2ff9446
Andrii Halabuda <losAngelesBoy11@gmail.com>
2017-03-31 16:08:11 +0300

    Updated UI

---
+++ losAngelesPlaces/LandmarksVC.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarksVC.swift
+//  losAngelesPlaces
+//
+//  Created by Andrew Foster on 3/31/17.
+//  Copyright © 2017 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class LandmarksVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationController?.isNavigationBarHidden = false
+    }
+
+
+}
---
+++ losAngelesPlaces/Material Design.swift
@@ -0,0 +1,48 @@
+//
+//  Material Design.swift
+//  random
+//
+//  Created by Andrew Foster on 11/8/16.
+//  Copyright © 2016 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+private var materialKey = false
+
+@IBDesignable
+extension UIView {
+    
+    @IBInspectable var materialDesign: Bool {
+        
+        get {
+            
+            return materialKey
+        }
+        
+        set {
+            
+            materialKey = newValue
+            
+            if materialKey {
+                
+                self.layer.masksToBounds = false
+                self.layer.cornerRadius = 15.0
+                self.layer.shadowOpacity = 0.8
+                self.layer.shadowRadius = 3.0
+                self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+                self.layer.shadowColor = UIColor(red: 45/255, green: 45/255, blue: 45/255, alpha: 1.0).cgColor
+                
+            } else {
+                
+                self.layer.cornerRadius = 0
+                self.layer.shadowOpacity = 0
+                self.layer.shadowRadius = 0
+                self.layer.shadowColor = nil
+            }
+            
+        }
+        
+    }
+    
+}
